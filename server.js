974862d16df3a714066c3a3adf83b88117354b04
Ahmedkn <ahmedkanoun03@gmail.com>
2023-07-14 13:59:04 +0100

    public version

---
+++ server.js
@@ -0,0 +1,45 @@
+const express=require('express');
+const app=express();
+const dotenv=require('dotenv');
+const cors=require('cors');
+const bodyParser=require('body-parser');
+const connectDB=require('./config/connectDB');
+const swaggerUi = require('swagger-ui-express'),
+swaggerDocument = require('./swagger/swagger.json');
+const path=require('path');
+//.env configuration
+dotenv.config();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({
+    extended:false
+}))
+
+//connect Database
+connectDB();
+
+//routes
+app.use(
+    '/api-docs',
+    swaggerUi.serve, 
+    swaggerUi.setup(swaggerDocument)
+  );
+app.use('/api/carousel',require('./Routes/carousel.js'));
+app.use('/api/accueil',require('./Routes/accueil'));
+app.use('/api/services',require('./Routes/services'));
+app.use('/api/lieux',require('./Routes/lieux'));
+app.use('/api/contact',require('./Routes/contact'));
+app.use('/api/social',require('./Routes/social'));
+
+//client
+const root=require('path').join(__dirname,'client','build');
+app.use(express.static(root));
+
+app.use('/*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','build','index.html'))
+})
+
+//Run Server
+app.listen(process.env.PORT || 6000,(err)=>err?console.log("can't start server"):console.log(`server running on port ${process.env.PORT || 6000}`));
